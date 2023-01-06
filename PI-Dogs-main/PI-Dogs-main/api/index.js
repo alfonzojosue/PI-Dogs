970250bbba6f47fb9bcff6a182a4b45613d2d23f
alfonzojosue <16-00436@usb.ve>
2023-01-06 11:28:23 -0400

    Deploy

--- PI-Dogs-main/PI-Dogs-main/api/index.js
+++ PI-Dogs-main/PI-Dogs-main/api/index.js
@@ -25,7 +25,7 @@ const {
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(DB_PORT, () => {
+  server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
