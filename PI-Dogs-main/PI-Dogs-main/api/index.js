30c74f703b4b0d4f2a38b4baa720147d43e75ee8
alfonzojosue <16-00436@usb.ve>
2023-01-06 23:39:49 -0400

    Deploy

--- PI-Dogs-main/PI-Dogs-main/api/index.js
+++ PI-Dogs-main/PI-Dogs-main/api/index.js
@@ -19,12 +19,10 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const {
-  PORT
-} = require('./config.js')
+
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(PORT, () => {
+  server.listen(process.env.PORT, () => {
     console.log('%s listening at', PORT); // eslint-disable-line no-console
   });
 });
