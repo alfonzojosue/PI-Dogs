2916a0bfd7884da64fd88896e915e4e15d0d0776
alfonzojosue <16-00436@usb.ve>
2023-01-12 13:04:16 -0400

    Deploy

--- PI-Dogs-main/PI-Dogs-main/api/index.js
+++ PI-Dogs-main/PI-Dogs-main/api/index.js
@@ -24,7 +24,7 @@ const {PORT} = require('./confing.js')
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(DB_PORT, () => {
+  server.listen(PORT, () => {
     console.log(`%s listening at `,PORT); // eslint-disable-line no-console
   });
 });
