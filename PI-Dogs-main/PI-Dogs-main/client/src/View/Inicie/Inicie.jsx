c3c95eb59143eb7a3ca14c64ddc92b7b6acb3045
alfonzojosue <16-00436@usb.ve>
2023-01-12 09:33:54 -0400

    Deploy

--- PI-Dogs-main/PI-Dogs-main/client/src/View/Inicie/Inicie.jsx
+++ PI-Dogs-main/PI-Dogs-main/client/src/View/Inicie/Inicie.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import style from './Inice.module.css'
 import patas from '../../imagen/pata.png'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
 
 const Inicie = () => {
-  
+ useEffect(() => {
+const options = {
+  method: 'GET',
+  url: 'https://anime-db.p.rapidapi.com/anime/by-ranking/2',
+  headers: {
+    'X-RapidAPI-Key': '9eb50b6ddemsh98383b5f04f46f5p10c1d5jsne8e032042dff',
+    'X-RapidAPI-Host': 'anime-db.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+ },[])
   return (
     <div className={style.inicie}>
       <div className={style.container}>
